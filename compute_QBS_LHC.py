cbff52c768b040403ac169d1b2e42d641cbd2277
LHC scrubbing <lhcscrub@lxplus0026.cern.ch>
2017-01-23 20:06:05 +0100

    Put iterative pressure calc to separate function

--- compute_QBS_LHC.py
+++ compute_QBS_LHC.py
@@ -18,6 +18,11 @@ import h5_storage
 
 zeros = lambda *x: np.zeros(shape=(x), dtype=float)
 dq_latest = data_qbs
+max_iterations = 5
+
+interp_P_T_hPT = interp2d(P,T,h_PT)
+interp_P_T_DPT = interp2d(P,T,D_PT)
+interp_P_T_mu = interp2d(P,T,mu_PT)
 
 def compute_qbs(atd_ob, use_dP, version=h5_storage.version, strict=True):
 
@@ -82,9 +87,6 @@ def compute_qbs(atd_ob, use_dP, version=h5_storage.version, strict=True):
     h3          = zeros(Nvalue,Ncell)    #enthalpy of BS output
     counter_int = np.zeros(shape=(Nvalue,Ncell),dtype=int)  #internal counter
      
-    interp_P_T_hPT = interp2d(P,T,h_PT)
-    interp_P_T_DPT = interp2d(P,T,D_PT)
-    interp_P_T_mu = interp2d(P,T,mu_PT)
     #interp_P_T_g = interp2d(P,T,gamma_PT) # not used
 
     P3 = np.copy(P1) #intermediate pressure ater the beam screen and before the valve
@@ -97,7 +99,6 @@ def compute_qbs(atd_ob, use_dP, version=h5_storage.version, strict=True):
     L_list = dq.L_list
     Qs_list = dq.Qs_list
 
-    max_iterations = 5
     for i in xrange(Ncell):
         for j in xrange(Nvalue):
             hC[j,i] = interp_P_T_hPT(P1[j,i],T1[j,i])
@@ -107,26 +108,32 @@ def compute_qbs(atd_ob, use_dP, version=h5_storage.version, strict=True):
 
         #compute the intermediate pressure P3 by iteration
         if use_dP:
-            for j in xrange(Nvalue):    #loop along the data values
-                while abs((P3_temp[j,i]-P3[j,i])/P3[j,i]) > 0.01 and counter_int[j,i] < max_iterations:
-                    #iterative loop to compute the pressure drop with error of 1% or 5 max iteration
-                    counter_int[j,i] += 1
-                    #protect against P3 < P4
-                    if P3[j,i] < P4[j,i]:
-                        P3[j,i] = P1[j,i]
-                        break
-                    elif P3_temp[j,i] != 0:
-                        P3[j,i] = P3_temp[j,i]
-                    m_L[j,i] = valve_LT(P3[j,i],P4[j,i],ro[j,i],gamma[j,i],Kv_list[i],CV[j,i],R_list[i])
-                    ro_dP[j,i] = interp_P_T_DPT(P3[j,i],(T2[j,i]+T3[j,i])/2.)
-                    mu[j,i] = interp_P_T_mu(P3[j,i],(T2[j,i]+T3[j,i])/2.)
-                    dP[j,i] = Pressure_drop(m_L[j,i]/nc_list[i],2*Radius, L_list[i], mu[j,i], ro_dP[j,i], rug)
-                    P3_temp[j,i] = P1[j,i] - dP[j,i];
-            
-                if P3[j,i] < P4[j,i]:
-                    P3[j,i] = P1[j,i]
-                else:
-                    P3[j,i] = P3_temp[j,i]
+            #for j in xrange(Nvalue):    #loop along the data values
+            #    while abs((P3_temp[j,i]-P3[j,i])/P3[j,i]) > 0.01 and counter_int[j,i] < max_iterations:
+            #        #iterative loop to compute the pressure drop with error of 1% or 5 max iteration
+            #        counter_int[j,i] += 1
+            #        #protect against P3 < P4
+            #        if P3[j,i] < P4[j,i]:
+            #            P3[j,i] = P1[j,i]
+            #            break
+            #        elif P3_temp[j,i] != 0:
+            #            P3[j,i] = P3_temp[j,i]
+            #        m_L[j,i] = valve_LT(P3[j,i],P4[j,i],ro[j,i],gamma[j,i],Kv_list[i],CV[j,i],R_list[i])
+            #        ro_dP[j,i] = interp_P_T_DPT(P3[j,i],(T2[j,i]+T3[j,i])/2.)
+            #        mu[j,i] = interp_P_T_mu(P3[j,i],(T2[j,i]+T3[j,i])/2.)
+            #        dP[j,i] = Pressure_drop(m_L[j,i]/nc_list[i],2*Radius, L_list[i], mu[j,i], ro_dP[j,i], rug)
+            #        P3_temp[j,i] = P1[j,i] - dP[j,i];
+            #
+            #    if P3[j,i] < P4[j,i]:
+            #        P3[j,i] = P1[j,i]
+            #    else:
+            #        P3[j,i] = P3_temp[j,i]
+            #    print('P1ij', P1[j,i])
+
+            for j in xrange(Nvalue):
+                P3[j,i], counter_int[j,i] = compute_P3(P1[j,i], P1[j,i], P4[j,i], T2[j,i], T3[j,i], Kv_list[i], CV[j,i], R_list[i], ro[j,i], gamma[j,i], nc_list[i], Radius, rug, L_list[i])
+
+                counter_int[j,i] = i
 
         m_L[:,i] = valve_LT(P3[:,i],P4[:,i],ro[:,i],gamma[:,i],Kv_list[i],CV[:,i],R_list[i])
         qbs[:,i] = m_L[:,i]*(h3[:,i]-hC[:,i])-Qs_list[i]-EH[:,i]
@@ -140,4 +147,39 @@ def compute_qbs(atd_ob, use_dP, version=h5_storage.version, strict=True):
         print('Warning: Maximum iterations were reached %i times!' % n_max_iterations)
 
     return tm.AlignedTimberData(atd_ob.timestamps, qbs, dq.Cell_list)
+
+def print_args(fn):
+    def new_fn(*args):
+        print(fn.__name__, args[0])
+        return fn(*args)
+    return new_fn
+
+#@print_args
+def compute_P3(P1, P3, P4, T2, T3, Kv, CV, R, ro, gamma, nc, Radius, rug, L):
+    P3_temp = 0
+    counter_int = 0
+    T_avg = (T2 + T3)/2.
+    while abs((P3_temp-P3)/P3) > 0.01 and counter_int < max_iterations:
+        #iterative loop to compute the pressure drop with error of 1% or 5 max iteration
+        counter_int += 1
+        #protect against P3 < P4
+        if P3 < P4:
+            P3 = P1
+            break
+        elif P3_temp != 0:
+            P3 = P3_temp
+        m_L = valve_LT(P3,P4,ro,gamma,Kv,CV,R)
+
+        ro_dP = interp_P_T_DPT(P3,T_avg)[0]
+        mu = interp_P_T_mu(P3,T_avg)[0]
+        dP = Pressure_drop(m_L/nc,2*Radius, L, mu, ro_dP, rug)
+        P3_temp = P1 - dP
+    
+    if P3 < P4:
+        P3 = P1
+    else:
+        P3 = P3_temp
+    return P3, counter_int
+
+
 
