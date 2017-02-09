8784308bce25dfae4f5f1e477f0d889cc032d0e3
giadarol <giovanni.iadarola@cern.ch>
2017-02-09 15:03:49 +0100

    changed paths

--- h5_storage.py
+++ h5_storage.py
@@ -6,20 +6,21 @@ import LHCMeasurementTools.myfilemanager as mfm
 
 # latest, default version
 version = 5
-h5_dir = '/eos/user/l/lhcscrub/timber_data_h5/cryo_heat_load_data/'
+h5_dir = '/eos/user/l/lhcscrub/timber_data_h5/'
+data_dir = h5_dir + '/cryo_heat_load_data/'
 
 def get_qbs_file(filln, version=version):
-    return h5_dir + 'recalculated_qbs_v%i/recalculated_qbs_v%i_%i.h5' % (version, version, filln)
+    return h5_dir + '/recalculated_qbs/recalculated_qbs_v%i/recalculated_qbs_v%i_%i.h5' % (version, version, filln)
 
 def get_data_file(filln):
-    return h5_dir + 'cryo_data_fill_%i.h5' % filln
+    return data_dir + '/cryo_data_fill_%i.h5' % filln
 
 def load_data_file(filln):
     ob =  mfm.h5_to_obj(get_data_file(filln))
     return tm.AlignedTimberData(ob.timestamps, ob.data, ob.variables)
 
 def get_special_data_file(filln):
-    return h5_dir + 'special_cells/special_data_fill_%i.h5' % filln
+    return data_dir + '/special_cells/special_data_fill_%i.h5' % filln
 
 def load_special_data_file(filln):
     ob = mfm.h5_to_obj(get_special_data_file(filln))
