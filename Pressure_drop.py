f8674ccfbe940b1adb1ec6f0dea207e257047b1a
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2017-02-13 14:15:28 +0100

    improved Pressure_drop

--- Pressure_drop.py
+++ Pressure_drop.py
@@ -4,9 +4,11 @@ import numpy as np
 def calc_fl(Re):
     return np.select([Re < 3e3, Re < 1e5, True], [64/Re, 0.3164/Re**0.25, 0.221/Re**0.237+0.0032])
 
+def calc_fr(D, rug):
+    return 1./((2.*np.log(D/(2*rug))+1.74))**2
+
 def pd_factory(D, rug):
-    fr = 1./((2.*np.log(D/(2*rug))+1.74))**2
-    print(fr)
+    fr = calc_fr(D,rug)
     R = D/2;
     S = np.pi*R**2
     def Pressure_drop(m, L, mu, rho):
@@ -14,7 +16,6 @@ def pd_factory(D, rug):
         Re = D*G/mu
         fl = calc_fl(Re)
         ff = np.where(fl>fr, fl, fr)
-
         return ff*m**2*L/(D*rho*S**2)*1e-5
 
     return Pressure_drop
@@ -22,6 +23,7 @@ def pd_factory(D, rug):
 if __name__ == '__main__':
     import argparse
     import matplotlib.pyplot as plt
+    from config_qbs import config_qbs
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', type=str, help='Path where to save the figure', default=None)
@@ -38,7 +40,7 @@ if __name__ == '__main__':
         single[ctr] = function(*arg)
     assert np.all(single == function(*args))
 
-    ln_Re = np.linspace(np.log(1e3), np.log(1e6), num=1000)
+    ln_Re = np.linspace(np.log(1e3), np.log(1e7), num=1000)
     Re = np.exp(ln_Re)
     fl = calc_fl(Re)
 
@@ -65,6 +67,8 @@ if __name__ == '__main__':
     sp.set_xscale('log')
     for xx in [3e3, 1e5]:
         sp.axvline(xx, color='black', lw=2)
+    fr = calc_fr(config_qbs.Radius, config_qbs.rug)
+    sp.axhline(fr, color='black', lw=2)
 
     if arguments.o is None:
         plt.show()
