c342cd9a622eca99b1699b60caebf97bfe06e2d2
LHC scrubbing <lhcscrub@lxplus0026.cern.ch>
2017-02-15 18:56:57 +0100

    tests equality of nested dicts with np arrays

---
+++ 011_test_aligned_to_dict.py
@@ -0,0 +1,34 @@
+import numpy as np
+import qbs_fill as qf
+
+filln = 5219
+
+def compare_dicts_recursively(dd1, dd2):
+    equal = True
+    for key, value1 in dd1.iteritems():
+        value2 = dd2[key]
+        if type(value1) is np.ndarray:
+            equal = np.all(value1 == value2)
+            if not equal:
+                print(value1[0], value2[0])
+                print key
+        elif type(value1) is dict:
+            equal = compare_dicts_recursively(value1, value2)
+        else:
+            equal = value1 == value2
+        if not equal:
+            break
+    return equal
+
+qbs_dict = qf.special_qbs_fill(5219)
+
+qbs_ob = qf.dict_to_aligned(qbs_dict)
+
+qbs_dict_2 = qf.aligned_to_dict(qbs_ob)
+print(compare_dicts_recursively(qbs_dict_2, qbs_dict))
+
+
+qbs_dict_2['timestamps'] = np.copy(qbs_dict['timestamps'])
+qbs_dict_2['timestamps'][0] = 0
+
+print(compare_dicts_recursively(qbs_dict_2, qbs_dict))
