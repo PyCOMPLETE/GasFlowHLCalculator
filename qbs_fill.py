269fec322b5790133baef9b3d7be900e81e58a19
LHC scrubbing <lhcscrub@lxplus0107.cern.ch>
2017-02-15 16:16:05 +0100

    merged giannis version

--- qbs_fill.py
+++ qbs_fill.py
@@ -4,14 +4,14 @@ import numpy as np
 import LHCMeasurementTools.TimberManager as tm
 import LHCMeasurementTools.LHC_Heatloads as HL
 import h5_storage
-from config_qbs import config_qbs, arc_index, arc_list
+from data_qbs import data_qbs, arc_index, arc_list
 from compute_QBS_special import compute_qbs_special
 import compute_QBS_LHC as cql
 
 version = h5_storage.version
 
 # Load data for one fill
-def compute_qbs_fill(filln, use_dP=True, version=version, recompute_if_missing=False):
+def compute_qbs_fill(filln, use_dP=True, version=version):
     """
     Arguments:
         -filln
@@ -23,9 +23,6 @@ def compute_qbs_fill(filln, use_dP=True, version=version, recompute_if_missing=F
         h5_file = h5_storage.get_qbs_file(filln, version)
         if os.path.isfile(h5_file):
             return h5_storage.load_qbs(filln, version=version)
-
-    if not recompute_if_missing:
-        raise ValueError('File does not exist. Set the recompute_if_missing flag!')
 
     atd_ob = h5_storage.load_data_file(filln)
     qbs_ob = cql.compute_qbs(atd_ob, use_dP, version=version)
@@ -55,43 +52,44 @@ def compute_qbs_arc_avg(qbs_ob):
     return tm.AlignedTimberData(qbs_ob.timestamps, qbs_arc_avg, arc_list)
 
 # plug-in replacement of old heat load procedure, the fill dict
-def get_fill_dict(filln_or_obj, version=version):
+def get_fill_dict(filln_or_obj):
     if isinstance(filln_or_obj, int):
-        qbs_ob = compute_qbs_fill(filln_or_obj, version=version)
+        qbs_ob = compute_qbs_fill(filln_or_obj)
     else:
         qbs_ob = filln_or_obj
+
+    # arcs
     qbs_arc_avg = compute_qbs_arc_avg(qbs_ob)
-
-    # Fill dict consists of timber_variable_list objects
-    def make_timber_variable_list(values):
-        tvl = tm.timber_variable_list()
-        tvl.t_stamps = qbs_ob.timestamps
-        tvl.ms = np.zeros_like(tvl.t_stamps)
-        tvl.values = values
-        return tvl
-
-    fill_dict = {}
-    # Arcs
+    output = {}
     for arc_ctr, arc in enumerate(arc_list):
         key = '%s_QBS_AVG_ARC.POSST' % arc
-        values = qbs_arc_avg.dictionary[arc]
-        fill_dict[key] = make_timber_variable_list(values)
+        tvl = tm.timber_variable_list()
+        tvl.t_stamps = qbs_arc_avg.timestamps
+        tvl.ms = np.zeros_like(tvl.t_stamps)
+        tvl.values = qbs_arc_avg.dictionary[arc]
+        output[key] = tvl
 
-    # Quads
-    quad_key_list = []
-    for key, list_ in HL.variable_lists_heatloads.iteritems():
-        if key[:2] in ('Q6', 'Q5'):
-            quad_key_list.extend(list_)
+    #others
+    varlist_tmb = []
+    for kk in HL.variable_lists_heatloads.keys():
+        varlist_tmb+=HL.variable_lists_heatloads[kk]
 
-    for ctr, cell in enumerate(qbs_ob.variables):
-        if cell[:2] in ('05', '06'):
-            cell = cell[:4]
-            for quad_key in quad_key_list:
-                if quad_key[6:10] == cell:
-                    fill_dict[quad_key] = make_timber_variable_list(qbs_ob.data[:,ctr])
-                    break
+    for varname in varlist_tmb:
+        #~ print varname
+        if '_Q1.' in varname: continue
+        if '_D2.' in varname: continue
+        if '_D3.' in varname: continue
+        if '_D4.' in varname: continue
+        if '_QBS9' in varname:
+            firstp, lastp = tuple(varname.split('_QBS'))
+            kkk = firstp.split('_')[-1]+'_'+lastp.split('.')[0]
+            tvl = tm.timber_variable_list()
+            tvl.t_stamps = qbs_ob.timestamps
+            tvl.ms = np.zeros_like(tvl.t_stamps)
+            tvl.values = qbs_ob.dictionary[kkk]
+            output[varname] = tvl
 
-    return fill_dict
+    return output
 
 def lhc_histograms(qbs_ob, avg_time, avg_pm, in_hrs=True):
     """
@@ -109,7 +107,7 @@ def lhc_histograms(qbs_ob, avg_time, avg_pm, in_hrs=True):
     varlist = []
     for ctr, arc in enumerate(arc_list):
         first, last = arc_index[ctr,:]
-        cell_names = config_qbs.Cell_list[first:last+1]
+        cell_names = data_qbs.Cell_list[first:last+1]
         mean = np.nanmean(qbs_ob.data[mask_mean,first:last+1], axis=0)
         mask_nan = np.logical_not(np.isnan(mean))
         arc_hist_dict[arc] = mean[mask_nan]
