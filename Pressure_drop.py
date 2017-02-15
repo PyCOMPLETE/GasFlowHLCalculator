8131d7f00000381a92b5be5b23b9e8ba26b6579a
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2017-02-15 16:19:27 +0100

    added nicer plot

--- Pressure_drop.py
+++ Pressure_drop.py
@@ -59,16 +59,20 @@ if __name__ == '__main__':
     fig.set_facecolor('w')
 
     sp = plt.subplot(1,1,1)
-    sp.plot(Re, fl, lw=2)
+    sp.plot(Re, fl, lw=2, label='f_L')
     #plt.title("Friction factor depending on Reynold's number")
     sp.grid(True)
     sp.set_ylabel('fl', fontsize=18)
     sp.set_xlabel('Re', fontsize=18)
     sp.set_xscale('log')
     for xx in [3e3, 1e5]:
-        sp.axvline(xx, color='black', lw=2)
+        sp.axvline(xx, color='black', lw=1)
+    for xx in [4.89e3, 4.389e5]:
+        sp.axvline(xx, color='red', lw=1)
     fr = calc_fr(config_qbs.Radius, config_qbs.rug)
-    sp.axhline(fr, color='black', lw=2)
+    sp.axhline(fr, color='green', lw=2, label='f_R')
+
+    sp.legend(loc=1)
 
     if arguments.o is None:
         plt.show()
