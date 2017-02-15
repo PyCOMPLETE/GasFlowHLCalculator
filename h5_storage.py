b32c0300ec2ca95d957aba12a2755f245b13b093
LHC scrubbing <lhcscrub@lxplus0026.cern.ch>
2017-02-15 17:20:56 +0100

    changed data_qbs to config_qbs

--- h5_storage.py
+++ h5_storage.py
@@ -4,32 +4,45 @@ import os
 import LHCMeasurementTools.TimberManager as tm
 import LHCMeasurementTools.myfilemanager as mfm
 
-# latest, default version
+# latest, default version (cell data only)
 version = 5
+
+# Directories
 h5_dir = '/eos/user/l/lhcscrub/timber_data_h5/'
 data_dir = h5_dir + '/cryo_heat_load_data/'
+special_data_dir = h5_dir + 'cryo_special_cell_data/'
 
+# Filenames for recomputed dada
 def get_qbs_file(filln, version=version):
     return h5_dir + '/recalculated_qbs/recalculated_qbs_v%i/recalculated_qbs_v%i_%i.h5' % (version, version, filln)
 
+def get_special_qbs_file(filln):
+    return h5_dir + '/recalculated_special_qbs/recalculated_special_qbs_%i.h5' % filln
+
+
+# Filename for raw data
 def get_data_file(filln):
     return data_dir + '/cryo_data_fill_%i.h5' % filln
 
+def get_special_data_file(filln):
+    return special_data_dir + '/special_data_fill_%i.h5' % filln
+
+
+# Load raw data
 def load_data_file(filln):
     ob =  mfm.h5_to_obj(get_data_file(filln))
     return tm.AlignedTimberData(ob.timestamps, ob.data, ob.variables)
-
-def get_special_data_file(filln):
-    return data_dir + '/special_cells/special_data_fill_%i.h5' % filln
 
 def load_special_data_file(filln):
     ob = mfm.h5_to_obj(get_special_data_file(filln))
     return tm.AlignedTimberData(ob.timestamps, ob.data, ob.variables)
 
+
+# Store recomputed data
 def store_qbs(filln, qbs_ob, use_dP, version=version):
     """
     Arguments:
-        -filln
+        - filln
         - qbs_ob
         - use_dP
         - version=version
@@ -46,7 +59,30 @@ def store_qbs(filln, qbs_ob, use_dP, version=version):
         qbs_dataset.attrs['with_dP'] = use_dP
         qbs_dataset.attrs['time_created'] = tm.UnixTimeStamp2UTCTimberTimeString(time.time())
 
+def store_special_qbs(filln, qbs_ob):
+    """
+    Arguments:
+        - filln
+        - qbs_ob
+    """
+    qbs_file = get_special_qbs_file(filln)
+    if not os.path.isdir(os.path.dirname(qbs_file)):
+        os.mkdir(os.path.dirname(qbs_file))
+
+    with h5py.File(qbs_file, 'w') as h5_handle:
+        h5_handle.create_dataset('timestamps', data=qbs_ob.timestamps)
+        h5_handle.create_dataset('variables', data=qbs_ob.variables)
+        qbs_dataset = h5_handle.create_dataset('data', data=qbs_ob.data)
+        qbs_dataset.attrs['time_created'] = tm.UnixTimeStamp2UTCTimberTimeString(time.time())
+
+# Load recomputed data
 def load_qbs(filln, version=version):
     qbs_file = get_qbs_file(filln, version)
     qbs_ob = mfm.h5_to_obj(qbs_file)
     return tm.AlignedTimberData(qbs_ob.timestamps, qbs_ob.data, qbs_ob.variables)
+
+def load_special_qbs(filln):
+    qbs_file = get_special_qbs_file(filln)
+    qbs_ob = mfm.h5_to_obj(qbs_file)
+    return tm.AlignedTimberData(qbs_ob.timestamps, qbs_ob.data, qbs_ob.variables)
+
