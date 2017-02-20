2dca4db78c9fd439353151353d0637fa7c357dc3
LHC scrubbing <lhcscrub@lxplus024.cern.ch>
2017-02-20 12:47:37 +0100

    minor

--- test_module.sh
+++ test_module.sh
@@ -7,7 +7,7 @@ python 005_test_qbs_lhc.py --noshow
 python valve_LT.py
 python Pressure_drop.py
 
-cd ..
+cd '/afs/cern.ch/work/l/lhcscrub/LHC_2016_25ns_beforeTS1'
 python ./027_special_instrumented_cells.py 5219
 python ./026_recalculate_heat_loads.py 5219
 python ./016_heat_load_arcs_vs_SAMs.py 5219
