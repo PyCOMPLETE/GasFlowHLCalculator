82b7ca9b9e5aacd1d0ffab884cb2d9bf3c8d7c25
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2017-06-01 09:48:51 +0200

    better routine to save variable lists

--- 002_qbs_variable_list.py
+++ 002_qbs_variable_list.py
@@ -1,7 +1,12 @@
-import sys
-if '..' not in sys.path: sys.path.append('..')
+import argparse
 
 import LHCMeasurementTools.myfilemanager as  mfm
+
+import data_S45_details as dd
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dry', help='Dry run', action='store_true')
+args = parser.parse_args()
 
 filln = 5219
 
@@ -11,6 +16,20 @@ atd = mfm.h5_to_obj(h5_filename)
 
 variable_list = atd.variables
 
-with open('./variable_list_complete.txt', 'w') as f:
-    f.write(','.join(variable_list))
+
+variable_list_special = set()
+
+for attr in dir(dd):
+    if '__' in attr: continue
+    name = getattr(dd, attr)
+    if type(name) is list:
+        for thing in name:
+            if type(thing) is str and 'POSST' in thing:
+                variable_list_special.add(thing)
+
+if not args.dry:
+    with open('./variable_list_complete.txt', 'w') as f:
+        f.write(','.join(variable_list))
+    with open('./variable_list_special.txt', 'w') as f:
+        f.write(','.join(list(variable_list_special)))
 
