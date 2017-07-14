d250b5828caafc0a620e35dafc3a511580b44dd2
LHC scrubbing <lhcscrub@lxplus042.cern.ch>
2017-07-14 19:54:19 +0200

    added versions to special cell heat loads

--- h5_storage.py
+++ h5_storage.py
@@ -6,6 +6,7 @@ import LHCMeasurementTools.myfilemanager as mfm
 
 # latest, default version (cell data only)
 version = 7
+special_version = 1
 
 # Directories
 h5_dir = '/eos/user/l/lhcscrub/timber_data_h5/'
@@ -18,12 +19,16 @@ def get_qbs_file(filln, use_dP, version=version):
     if use_dP:
         return recalc_dir + 'recalculated_qbs_v%i/recalculated_qbs_v%i_%i.h5' % (version, version, filln)
     else:
+        # version 6->7 only changed things for with dP
         if version == 7:
             version = 6
         return recalc_dir + '/recalculated_qbs_nodP_v%i/recalculated_qbs_nodP_v%i_%i.h5' % (version, version, filln)
 
-def get_special_qbs_file(filln):
-    return h5_dir + '/recalculated_special_qbs/recalculated_special_qbs_%i.h5' % filln
+def get_special_qbs_file(filln, special_version=special_version):
+    if special_version == 0:
+        return h5_dir + '/recalculated_special_qbs/recalculated_special_qbs_v%i/recalculated_special_qbs_%i.h5' % (special_version, filln)
+    else:
+        return h5_dir + '/recalculated_special_qbs/recalculated_special_qbs_v%i/recalculated_special_qbs_v%i_%i.h5' % (special_version, special_version, filln)
 
 
 # Filename for raw data
@@ -47,13 +52,14 @@ def load_special_data_file(filln):
 def load_qbs(filln, use_dP, version=version):
     qbs_file = get_qbs_file(filln, version=version, use_dP=use_dP)
     qbs_ob = mfm.h5_to_obj(qbs_file)
-    print('Loaded file %s' % qbs_file)
+    #print('Loaded file %s' % qbs_file)
     return tm.AlignedTimberData(qbs_ob.timestamps, qbs_ob.data, qbs_ob.variables)
 
-def load_special_qbs(filln):
-    qbs_file = get_special_qbs_file(filln)
+def load_special_qbs(filln, special_version=special_version):
+    qbs_file = get_special_qbs_file(filln, special_version=special_version)
     qbs_ob = mfm.h5_to_obj(qbs_file)
     return tm.AlignedTimberData(qbs_ob.timestamps, qbs_ob.data, qbs_ob.variables)
+
 
 # Store recomputed data
 def store_qbs(filln, qbs_ob, use_dP, version=version):
@@ -76,13 +82,14 @@ def store_qbs(filln, qbs_ob, use_dP, version=version):
         qbs_dataset.attrs['with_dP'] = use_dP
         qbs_dataset.attrs['time_created'] = tm.UnixTimeStamp2UTCTimberTimeString(time.time())
 
-def store_special_qbs(filln, qbs_ob):
+def store_special_qbs(filln, qbs_ob, special_version=special_version):
     """
     Arguments:
         - filln
         - qbs_ob
+        - special_version=special_version
     """
-    qbs_file = get_special_qbs_file(filln)
+    qbs_file = get_special_qbs_file(filln, special_version=special_version)
     if not os.path.isdir(os.path.dirname(qbs_file)):
         os.mkdir(os.path.dirname(qbs_file))
 
