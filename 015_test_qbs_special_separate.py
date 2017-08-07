0109b3aac742d432d70a19e74fb26bbae4d3d31c
LHC scrubbing <lhcscrub@lxplus014.cern.ch>
2017-08-07 18:36:15 +0200

    improved 015 script

--- 015_test_qbs_special_separate.py
+++ 015_test_qbs_special_separate.py
@@ -1,11 +1,13 @@
 from __future__ import division
+import argparse
 import matplotlib.pyplot as plt
 
 import LHCMeasurementTools.mystyle as ms
+import LHCMeasurementTools.savefig as sf
 
 import h5_storage
 import compute_QBS_special as cqs
-from data_S45_details import cell_timber_vars_dict
+#from data_S45_details import cell_timber_vars_dict
 
 #(30-07-17)
 #
@@ -20,10 +22,18 @@ from data_S45_details import cell_timber_vars_dict
 # --> Solved by excluding the last magnet from the calculation, and also adding 31L2 to the normal gasflow list as the temp sensors are reversed (826 instead of 824).
 # To be checked with B.B.
 
+parser = argparse.ArgumentParser()
+parser.add_argument('filln', help='LHC fill number', type=int)
+parser.add_argument('--savefig', help='Save in file.', action='store_true')
+parser.add_argument('--noshow', help='Do not call plt.show', action='store_true')
+args = parser.parse_args()
+
+
+
 plt.close('all')
 ms.mystyle(10)
 
-filln = 5885
+filln = args.filln
 
 blacklist = [
     ('13L5', 'D3'),
@@ -44,38 +54,52 @@ tt = (atd.timestamps - atd.timestamps[0])/3600
 sp = None
 for cell_ctr, cell in enumerate(special_dict_separate['cells']):
     fig = ms.figure(cell)
-    sp1 = sp = plt.subplot(2,2,1, sharex=sp)
-    sp1.set_title('Sum of magnet Fill %i' % filln)
-    sp2 = sp = plt.subplot(2,2,2, sharex=sp)
-    sp2.set_title('Separate beams Fill %i' % filln)
+    sp1 = sp = plt.subplot(2,2,2, sharex=sp)
+    sp1.set_title('Cell %s - sum of magnet Fill %i' % (cell, filln))
+    sp2 = sp = plt.subplot(2,2,1, sharex=sp)
+    sp2.set_title('Cell %s - separate beams Fill %i' % (cell, filln))
+
+    for sp in (sp1, sp2):
+        sp.set_xlabel('Time [h]')
+        sp.set_ylabel('Heat load [W]')
 
 
     label_ctr = 0
     for magnet_ctr, magnet_id in enumerate(cqs.magnet_ids):
         if (cell, magnet_id) in blacklist: continue
-        if (cell_timber_vars_dict[cell]['first_element'], magnet_id) in [('D4', 'Q1') or ('Q1', 'D4')]: continue
+        #if (cell_timber_vars_dict[cell]['first_element'], magnet_id) in [('D4', 'Q1') or ('Q1', 'D4')]: continue
         label_ctr += 1
         if label_ctr == 1:
             label_old, label_separate = 'Combined', 'Sum of beams'
         else:
             label_old, label_separate = None, None
         color = ms.colorprog(magnet_ctr, cqs.magnet_ids)
-        sp1.plot(tt, special_dict_not_separate[cell][magnet_id], color=color, label=label_old)
+        sp1.plot(tt, special_dict_not_separate[cell][magnet_id], color=color, label=label_old, lw=2)
         sum_magnet = 0
         for beam in (1,2):
             label = magnet_id
             if label_ctr == 1:
                 label += ' Beam %i' % beam
+            elif beam == 2:
+                label = None
             hl_beam = special_dict_separate[cell][magnet_id][beam]
             sum_magnet += hl_beam
             ls = ['-',':'][beam-1]
-            sp2.plot(tt, hl_beam, ls=ls, color=color, label=label)
-        sp1.plot(tt, sum_magnet, color=color, ls=':', label=label_separate)
+            sp2.plot(tt, hl_beam, ls=ls, color=color, label=label, lw=2)
+        sp1.plot(tt, sum_magnet, color=color, ls=':', label=label_separate, lw=2)
 
     for sp in sp1, sp2:
         sp.grid(True)
-        sp.legend()
+        sp.legend(loc=0)
         sp.set_ylim(-5, None)
 
-plt.show()
+if args.savefig:
+    for num in plt.get_fignums():
+        fig = plt.figure(num)
+        plt.suptitle('')
+    sf.saveall_pdijksta()
+
+
+if not args.noshow:
+    plt.show()
 
