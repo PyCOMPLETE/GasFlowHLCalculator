ba531650cc5bd9f319f7587dfc697e28c63c5eea
LHC scrubbing <lhcscrub@lxplus060.cern.ch>
2017-10-05 17:06:44 +0200

    before removing 014

--- 014_new_config_format.py
+++ 014_new_config_format.py
@@ -1,22 +1,21 @@
-
 with open('./LHCCryoHeatLoadCalibration/CryoBeamScreenData.csv') as f:
     f_new = f.readlines()
-    f_new = map(lambda x: x.split(','), f_new)
+    f_new = [x.split(',') for x in f_new]
 
 with open('./config_qbs_lhc_3.csv') as f:
     f_old = f.readlines()
-    f_old = map(lambda x: x.split(), f_old)
+    f_old = [x.split() for x in f_old]
 
 
+print('Different naming:\n')
 for i in xrange(len(f_new[0])):
-    #print f_new[0][i], f_new[1][i]
         for j in xrange(len(f_old[0])):
             if f_old[1][j] == f_new[1][i]:
                 print f_old[0][j], f_new[0][i]
 
 
 cell_names = set()
-print "duplicated"
+print('\nDuplicated cells in new config:\n')
 for i, line in enumerate(f_new):
     cell_name = line[3]
     if cell_name in cell_names:
