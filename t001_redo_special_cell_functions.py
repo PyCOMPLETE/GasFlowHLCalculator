7591dd6f652ed2e814318576fab842de4899cd00
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2019-09-28 15:36:42 +0200

    Start modularizing

--- t001_redo_special_cell_functions.py
+++ t001_redo_special_cell_functions.py
@@ -5,6 +5,104 @@ import Helium_properties as hp
 from valve_LT import valve_LT
 from Pressure_drop import pd_factory
 
+
+# Functions
+def compute_heat_load(P1, T1, T3, P4, CV, EH, Qs_calib, Kv_calib, R_calib,
+        cell_length, n_channels, channel_radius, channel_roughness,
+        with_P_drop=True, N_iter_max=100, scale_correction=0.3,
+        iter_toll=1e-3):
+
+    # Evaluate enthalpy at circuit entrance
+    H1 = hp.interp_P_T_hPT(P1, T1)
+
+    # We initially neglect the pressure drop
+    P3_0 = P1.copy()
+
+    # Evaluate enthalpy at circuit exit 
+    H3_0 = hp.interp_P_T_hPT(P3_0, T3)
+
+    # Evaluate density at valve
+    rho = hp.interp_P_T_DPT(P3_0, T3)
+
+    # Evaluate the mass flow from the valve characteristics
+    m_L_0 = valve_LT(pin=P3_0, pout=P4, rho=rho, kv=Kv_calib,
+            u=CV, R=R_calib)
+
+    # Estimate pressure drop DP = P1 - P3
+    pressure_drop = pd_factory(D=2*channel_radius,
+                               rug=channel_roughness)
+
+    P3 = P3_0.copy()
+    P3_list = []
+
+    DP_prev = 0.
+    mask_iter = P3 > P4
+    for i_iter in range(N_iter_max):
+
+        P3_prev_iter = P3[mask_iter].copy()
+        DP_prev_iter = P1[mask_iter] - P3_prev_iter
+
+        m_L_iter = valve_LT(pin=P3[mask_iter], pout=P4[mask_iter], rho=rho[mask_iter],
+                kv=Kv_calib, u=CV[mask_iter], R=R_calib)
+
+        #H3_iter = H3_0[mask_iter]
+        H3_iter = hp.interp_P_T_hPT(P3[mask_iter], T3[mask_iter])
+        H2_iter = (m_L_iter * H1[mask_iter] + EH[mask_iter]) / m_L_iter
+        H_ave_iter = 0.5*(H2_iter + H3_iter)
+        rho_DP_iter = hp.interp_P_H_DPH(P3[mask_iter], H_ave_iter)
+        mu_iter = hp.inperp_P_H_mu(P3[mask_iter], H_ave_iter)
+
+        #T_ave = (T2 + T3)/2.
+        #rho_DP = hp.interp_P_T_DPT(P3, T_ave)
+        #mu = hp.interp_P_T_mu(P3, T_ave)
+
+        DP_new_iter = pressure_drop(m=m_L_iter/n_channels,
+                        L=cell_length, mu=mu_iter , rho=rho_DP_iter)
+
+        DP_iter = DP_prev_iter + scale_correction * (DP_new_iter - DP_prev_iter)
+
+        P3_iter = P1[mask_iter] - DP_iter
+
+        # Identify negative (P3-P4)
+        mask_negative_iter = P3_iter < P4[mask_iter]
+
+        # Stop iteration for negative values
+        mask_iter[mask_iter][mask_negative_iter] = False
+
+        # Update only positive (P3-P4)  
+        P3[mask_iter] = P3_iter[~mask_negative_iter]
+
+        # Stop iteration for point where convergence is found
+        mask_iter[mask_iter] = (np.abs((P3_iter[~mask_negative_iter] \
+                - P3_prev_iter[~mask_negative_iter])\
+                 / P3_prev_iter[~mask_negative_iter]) > iter_toll)
+
+        if np.sum(mask_iter) == 0:
+            break
+
+        P3_list.append(P3.copy())
+
+    P3_list = np.array(P3_list)
+
+    # Re-evaluate mass flow
+    m_L = valve_LT(pin=P3, pout=P4, rho=rho, kv=cell_calibration['Kv'],
+            u=CV, R=cell_calibration['R'])
+
+    # Re-evaluate H3
+    H3 = hp.interp_P_T_hPT(P3, T3)
+
+    # Compute heat load
+    Q_bs = m_L * (H3 - H1) - cell_calibration['Qs'] - EH
+
+    other = {
+        'H1': H1,
+        'H3': H3,
+        'P3': P3,
+        'mass_flow': m_L
+        }
+
+    return Q_bs, other
+
 cell_description = {
     'QBS': 'QRLAB_31L2_QBS943.POSST',
 
@@ -64,95 +162,27 @@ EH = obraw.dictionary[cell_description['EH']]
 
 # T2 = obraw.dictionary[cell_description['T2']]
 
-# Evaluate enthalpy at circuit entrance
-H1 = hp.interp_P_T_hPT(P1, T1)
-
-# We initially neglect the pressure drop
-P3_0 = P1.copy()
-
-# Evaluate enthalpy at circuit exit 
-H3_0 = hp.interp_P_T_hPT(P3_0, T3)
-
-# Evaluate density at valve
-rho = hp.interp_P_T_DPT(P3_0, T3)
-
-# Evaluate the mass flow from the valve characteristics
-m_L_0 = valve_LT(pin=P3_0, pout=P4, rho=rho, kv=cell_calibration['Kv'],
-        u=CV, R=cell_calibration['R'])
-
-# Estimate pressure drop DP = P1 - P3
-pressure_drop = pd_factory(D=2*cell_description['channel_radius'],
-                           rug=cell_description['roughness'])
-
-P3 = P3_0.copy()
-P3_list = []
-
-DP_prev = 0.
-N_iter_max = 100
-scale_correction = 0.3
-
-mask_iter = P3 > P4
-
-for i_iter in range(N_iter_max):
-
-    P3_prev_iter = P3[mask_iter].copy()
-    DP_prev_iter = P1[mask_iter] - P3_prev_iter
-
-    m_L_iter = valve_LT(pin=P3[mask_iter], pout=P4[mask_iter], rho=rho[mask_iter],
-            kv=cell_calibration['Kv'], u=CV[mask_iter], R=cell_calibration['R'])
-
-    #H3_iter = H3_0[mask_iter]
-    H3_iter = hp.interp_P_T_hPT(P3[mask_iter], T3[mask_iter])
-    H2_iter = (m_L_iter * H1[mask_iter] + EH[mask_iter]) / m_L_iter
-    H_ave_iter = 0.5*(H2_iter + H3_iter)
-    rho_DP_iter = hp.interp_P_H_DPH(P3[mask_iter], H_ave_iter)
-    mu_iter = hp.inperp_P_H_mu(P3[mask_iter], H_ave_iter)
-
-    #T_ave = (T2 + T3)/2.
-    #rho_DP = hp.interp_P_T_DPT(P3, T_ave)
-    #mu = hp.interp_P_T_mu(P3, T_ave)
-
-    DP_new_iter = pressure_drop(m=m_L_iter/cell_description['n_channels_tot'],
-                    L=cell_description['length'], mu=mu_iter , rho=rho_DP_iter)
-
-    DP_iter = DP_prev_iter + scale_correction * (DP_new_iter - DP_prev_iter)
-
-    P3_iter = P1[mask_iter] - DP_iter
-
-    # Identify negative (P3-P4)
-    mask_negative_iter = P3_iter < P4[mask_iter]
-
-    # Stop iteration for negative values
-    mask_iter[mask_iter][mask_negative_iter] = False
-
-    # Update only positive (P3-P4)  
-    P3[mask_iter] = P3_iter[~mask_negative_iter]
-
-    # Stop iteration for point where convergence is found
-    mask_iter[mask_iter] = (np.abs((P3_iter[~mask_negative_iter] \
-            - P3_prev_iter[~mask_negative_iter])\
-             / P3_prev_iter[~mask_negative_iter]) > 0.001)
-
-    if np.sum(mask_iter) == 0:
-        break
-
-    P3_list.append(P3.copy())
+Q_bs, other = compute_heat_load(P1, T1, T3, P4, CV, EH,
+        Qs_calib=cell_calibration['Qs'],
+        Kv_calib=cell_calibration['Kv'],
+        R_calib=cell_calibration['R'],
+        cell_length=cell_description['length'],
+        n_channels=cell_description['n_channels_tot'],
+        channel_radius=cell_description['channel_radius'],
+        channel_roughness=cell_description['roughness'],
+        with_P_drop=True, N_iter_max=100, scale_correction=0.3,
+        iter_toll=1e-3)
 
 
-P3_list = np.array(P3_list)
 
-# Re-evaluate mass flow
-m_L = valve_LT(pin=P3, pout=P4, rho=rho, kv=cell_calibration['Kv'],
-        u=CV, R=cell_calibration['R'])
-
-# Re-evaluate H3
-H3 = hp.interp_P_T_hPT(P3, T3)
-
-# Compute heat load
-Q_bs = m_L * (H3 - H1) - cell_calibration['Qs'] - EH
 
 #####################################################################
 # Compute share between the two beam screens
+N_iter_max = 100
+m_L = other['mass_flow']
+
+pressure_drop = pd_factory(D=2*cell_description['channel_radius'],
+                           rug=cell_description['roughness'])
 
 n_channels_circuits = [cell_description['n_channels_circuit_%s'%cc]
                             for cc in ['A', 'B']]
