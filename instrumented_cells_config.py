d40cbe4f7f9481bbea6656d9d23d8ec1c7100840
giadarol <giovanni.iadarola@cern.ch>
2019-10-01 09:35:48 +0200

    Another instrumented cell

--- instrumented_cells_config.py
+++ instrumented_cells_config.py
@@ -14,7 +14,7 @@ instrumented_cells_config = {
         'n_channels_circuit_B': 2,
 
         'magnet_names': ['D4', 'D3', 'D2', 'Q1'],
-        'magnet_lengths': [5.9, 15.7, 15.7, 15.7],
+        'magnet_lengths': [15.7, 15.7, 15.7, 5.9],
 
         'circuit_A_beam': [1,2,1,2],
         'circuit_B_beam': [2,1,2,1],
@@ -34,6 +34,26 @@ instrumented_cells_config = {
         'n_channels_circuit_B': 2,
 
         'magnet_names': ['D4', 'D3', 'D2', 'Q1'],
+        'magnet_lengths': [15.7, 15.7, 15.7, 5.9],
+
+        'circuit_A_beam': [1,2,1,2],
+        'circuit_B_beam': [2,1,2,1],
+
+    },
+
+    'QRLAA_13R4_QBS947.POSST': {
+
+        'circuit_A_sensors': ['QBQI_12R4_TT825.POSST', 'QQBI_12R4_TT824.POSST',
+                    'QBBI_A13R4_TT826.POSST', 'QBBI_B13R4_TT824.POSST',
+                    'QBQI_13R4_TT825.POSST'],
+        'circuit_B_sensors': ['QBQI_12R4_TT825.POSST', 'QQBI_12R4_TT826.POSST',
+                    'QBBI_A13R4_TT824.POSST', 'QBBI_B13R4_TT826.POSST',
+                    'QBQI_13R4_TT825.POSST'],
+
+        'n_channels_circuit_A': 2,
+        'n_channels_circuit_B': 2,
+
+        'magnet_names': ['Q1', 'D2', 'D3', 'D4'],
         'magnet_lengths': [5.9, 15.7, 15.7, 15.7],
 
         'circuit_A_beam': [1,2,1,2],
