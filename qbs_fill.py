be3511a6559e087e054bfa6012dca11d854ab9e3
LHC ecloud <lhcecld@lxplus719.cern.ch>
2022-09-07 17:47:00 +0200

    Omit instrumented cells if the data is not there.

--- qbs_fill.py
+++ qbs_fill.py
@@ -5,13 +5,20 @@ import LHCMeasurementTools.myfilemanager as mfm
 # plug-in replacement of old heat load procedure, the fill dict
 def get_fill_dict(filln, h5_storage=None, use_dP=True):
 
-    obhl =  h5_storage.load_qbs(filln, use_dP=use_dP)
-    obhl_instrum =h5_storage.load_special_qbs(filln)
+    obhl_list = []
+
+    obhl = h5_storage.load_qbs(filln, use_dP=use_dP)
+    obhl_list.append(obhl)
+    try:
+        obhl_instrum = h5_storage.load_special_qbs(filln)
+        obhl_list.append(obhl_instrum)
+    except FileNotFoundError as err:
+        print('Instrumented cell heat loads not found: ', err)
 
     ms0 = 0.*obhl.timestamps
 
     dict_out = {}
-    for ob in [obhl, obhl_instrum]:
+    for ob in obhl_list:
         for ii, vv in enumerate(ob.variables):
             tv = tm.timber_variable_list()
             tv.t_stamps = ob.timestamps
